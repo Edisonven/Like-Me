8a6dca8bd5752160afe4c65b723ada49359e176a
Edison <aalevenegass@gmail.com>
2024-04-18 13:00:21 -0400

    Agregando método post al backend

--- backend_app/server/index.js
+++ backend_app/server/index.js
@@ -19,11 +19,27 @@ app.get("/posts", async (req, res) => {
     const query = "SELECT * FROM posts;";
     const { rows } = await pool.query(query);
     res.json(rows);
+    console.log("mostrando todos los posts", rows);
   } catch (error) {
     console.log("ha ocurrido un error", error);
     res
       .status(500)
       .json({ error: "Ha ocurrido un error al procesar tu solicitud" });
+  }
+});
+
+app.post("/posts", async (req, res) => {
+  try {
+    const { titulo, url, descripcion } = req.body;
+    const id = Math.floor(Math.random() * 9999);
+    const query =
+      "INSERT INTO posts (id, titulo, img, descripcion, likes) VALUES ($1, $2, $3, $4, $5);";
+    const values = [id, titulo, url, descripcion, 0];
+    const { rows } = await pool.query(query, values);
+    res.json("post agregado con éxito");
+  } catch (error) {
+    console.log("ha ocurrido un error al postear");
+    res.status(400).json({ message: "error al procesar la operación " });
   }
 });
 
