1121596297929d7fb513dac399753c9d8ea59b92
Edison <aalevenegass@gmail.com>
2024-04-24 14:45:17 -0400

    Agregando controlador para las rutas

--- backend_app/Routes/route.js
+++ backend_app/Routes/route.js
@@ -1,58 +1,13 @@
-import { postModel } from "../models/model.js";
+import { postController } from "../controllers/controller.js";
 import { Router } from "express";
 const router = Router();
 
-router.get("/", async (req, res) => {
-  try {
-    const posts = await postModel.findAll();
-    res.json(posts);
-    console.log("mostrando todos los posts");
-  } catch (error) {
-    console.log("ha ocurrido un error", error);
-    res
-      .status(500)
-      .json({ error: "Ha ocurrido un error al procesar tu solicitud" });
-  }
-});
+router.get("/", postController.read);
 
-router.post("/", async (req, res) => {
-  try {
-    const { titulo, url, descripcion } = req.body;
-    if (!titulo || !url || !descripcion) {
-      console.log("No pueden haber campos vacíos");
-      return;
-    }
-    const posts = await postModel.create(titulo, url, descripcion);
-    res.json(posts);
-  } catch (error) {
-    console.log("ha ocurrido un error al postear");
-    res.status(400).json({ message: "error al procesar la operación " });
-  }
-});
+router.post("/", postController.create);
 
-router.put("/like/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const posts = postModel.update(id);
-    res.json(posts);
-  } catch (error) {
-    console.log("ha ocurrido un error", error);
-    res
-      .status(400)
-      .json({ error: "Ha ocurrido un error al procesar tu solicitud" });
-  }
-});
+router.put("/like/:id", postController.update);
 
-router.delete("/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const posts = postModel.remove(id);
-    res.json(posts);
-  } catch (error) {
-    res
-      .status(400)
-      .json({ error: "Ha ocurrido un error al procesar tu solicitud" });
-  }
-});
+router.delete("/:id", postController.remove);
 
 export default router;
---
+++ backend_app/controllers/controller.js
@@ -0,0 +1,50 @@
+import { postModel } from "../models/model.js";
+
+const read = async (req, res) => {
+  try {
+    const posts = await postModel.findAll();
+    return res.json(posts);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+const create = async (req, res) => {
+  try {
+    const { titulo, url, descripcion } = req.body;
+    if (!titulo || !url || !descripcion) {
+      console.log("No pueden haber campos vacíos");
+      return;
+    }
+    const posts = await postModel.create(titulo, url, descripcion);
+    res.json(posts);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+const update = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const posts = postModel.update(id);
+    res.json(posts);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+const remove = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const posts = postModel.remove(id);
+    res.json(posts);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const postController = { read, create, update, remove };
