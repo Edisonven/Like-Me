1382cc437e75914b533b9bd693cf337be283dbdd
Edison <aalevenegass@gmail.com>
2024-04-24 18:37:01 -0400

    Añadiendo favicon en proyecto

--- backend_app/server/index.js
+++ backend_app/server/index.js
@@ -9,5 +9,5 @@ app.use("/posts", postRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server listening on port http://localhost:${PORT}`);
+  console.log(`Puerto funcionando en: http://localhost:${PORT}`);
 });
